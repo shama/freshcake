04f3a68186ecd551d8fce02a4c155d964214b86a
shama <kyle@kyletyoung.com>
2010-10-07 15:49:56 -0700

    added database example

---
+++ config/database.example.php
@@ -0,0 +1,10 @@
+<?php
+class DATABASE_CONFIG {
+	
+	var $freshbooks = array(
+		'datasource' => 'freshbooks.freshbooks',
+		'subdomain' => 'YOUR-SUBDOMAIN-HERE',
+		'token' => 'YOUR-TOKEN-HERE',
+	);
+	
+}
